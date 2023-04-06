11e6e45484f22061b23a79c39c136530a74a5b6a
maxingfang <maxingfang@meituan.com>
2023-04-06 14:44:02 +0800

    基本使用: 函数式编程

--- src/main/kotlin/com/turreta/maven/kotlin/lambda/Main.kt
+++ src/main/kotlin/com/turreta/maven/kotlin/lambda/Main.kt
@@ -2,10 +2,48 @@ package com.turreta.maven.kotlin.lambda
 
 fun main() {
     // 集合类的常规使用（创建和遍历）
-    // testCollectionNormal()
+    testCollectionNormal()
 
     // 集合的函数式 API
     testCollectionFunApi()
+
+    // Java 函数式 API 的使用
+    testJavaFunApi();
+
+}
+
+fun testJavaFunApi() {
+    //Kotlin 中可以这么写(实际上是个Object）
+    Thread(object : Runnable {
+        override fun run() {
+            println(Thread.currentThread().toString())
+        }
+    }).start()
+
+    /**
+     * 我们接着来简化 Kotlin 中的写法
+     * 因为 Runnable 类中只有一个待实现方法，即使这里没有显示的重写 run() 方法，
+     * Kotlin 也能明白后面的 Lambda 表达式就是要在 run() 方法中实现的内容
+     */
+    Thread(Runnable {
+        println(Thread.currentThread().toString())
+    }).start()
+
+    //因为是单抽象方法接口，我们可以将接口名进行省略
+    Thread({
+        println(Thread.currentThread().toString())
+    }).start()
+
+    //当 Lambda 表达式作为函数的最后一个参数的时候，我们可以把 Lambda 表达式移到函数括号的外面
+    Thread() {
+        println(Thread.currentThread().toString())
+    }.start()
+
+    //当 Lambda 表达式是函数的唯一参数的时候，函数的括号可以省略
+    Thread {
+        println(Thread.currentThread().toString())
+    }.start()
+
 
 }
 
@@ -27,15 +65,39 @@ fun testCollectionFunApi(): Unit {
 
     //但是如果使用函数式 Api 将会变得更加简单, maxBy 函数会根据你的条件遍历得到符合条件的最大值
     //方式2
-//    maxLengthFruit = list1.maxBy({ it ->
-//        it.length
-//    })
-
     maxLengthFruit = list1.maxBy { it.length }
     print(maxLengthFruit)
 
     println()
 
+    val lambda = { fruit: String -> fruit.length }
+    //maxBy 函数实际上接收的是一个函数类型的参数，后续讲高阶函数的时候会讲到，也就是我们这里可以传入一个 Lambda 表达式
+    maxLengthFruit = list1.maxBy(lambda)
+
+    //2 替换 lambda
+    maxLengthFruit = list1.maxBy({ fruit: String -> fruit.length })
+
+    //3 Kotlin 中规定，当 Lambda 表达式作为函数的最后一个参数的时候，我们可以把 Lambda 表达式移到函数括号的外面
+    maxLengthFruit = list1.maxBy() { fruit: String -> fruit.length }
+
+    //4 Kotlin 中规定，当 Lambda 表达式是函数的唯一参数的时候，函数的括号可以省略
+    maxLengthFruit = list1.maxBy { fruit: String -> fruit.length }
+
+    //5 当 Lambda 表达式的参数列表中只有一个参数的时候，我们可以把参数给省略，默认会有个 it 参数
+    maxLengthFruit = list1.maxBy { it.length }
+
+    print(maxLengthFruit)
+
+    println()
+
+    //通过 filter 筛选操作，筛选长度小于等于5的字符串
+    val newList = list1.filter {
+        it.length <= 5
+    }
+    for (s in newList) {
+        print("$s ")
+    }
+    println()
 
 }
 
